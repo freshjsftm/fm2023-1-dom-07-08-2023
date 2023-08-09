bf83d4d122393abf639358cb27e59889b834692b
Olena Zdanovska <freshjs@gmail.com>
2023-08-09 11:03:45 +0300

    Add work with Attr

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,28 +1,13 @@
 'use strict';
 
 const unique = document.getElementById('unique');
-const section = unique.parentElement;
 
-const log = (event) => {
-  event.preventDefault();
-  event.stopPropagation();
-  console.group();
-  console.log(event.target); //на кому згенерувалася подія
-  console.log(event.currentTarget); //чий обробник працює
-  console.groupEnd();
-  event.target.removeEventListener('click', log);
-};
+// unique.setAttribute('title', 'new title text');
 
-unique.addEventListener('click', log);
-section.addEventListener('click', log);
-document.body.addEventListener('click', log, false);
-// document.addEventListener('click', log);
-window.addEventListener('click', log, { capture: false, once: true });
+// if (unique.hasAttribute('ref')) {
+//   unique.removeAttribute('ref');
+// }
 
-// unique.addEventListener(
-//   'click',
-//   (event) => {
-//     console.log('event');
-//   },
-//   { once: true }
-// );
+const attrTitle = document.createAttribute('title');
+attrTitle.value = 'new title text for attr';
+unique.setAttributeNode(attrTitle);
