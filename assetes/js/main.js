749cb6165b8c0a60f45faff503c344ea5ddd9c45
Olena Zdanovska <freshjs@gmail.com>
2023-08-09 11:34:04 +0300

    Add dataset

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,13 +1,31 @@
 'use strict';
 
-const unique = document.getElementById('unique');
+const btns = document.querySelectorAll('section>button');
 
-// unique.setAttribute('title', 'new title text');
-
-// if (unique.hasAttribute('ref')) {
-//   unique.removeAttribute('ref');
+// for (const btn of btns) {
+//   btn.addEventListener('click', (event) => {
+//     // console.log(event.target.textContent);
+//     // console.log(event.target.innerText);
+//     // console.log(event.target.innerHTML);
+//     // console.dir(event.target.attributes);
+//     // console.log(event.target.dataset.color);
+//     //console.log(event.target.style);
+//     //прописати фоной колір секції
+//     event.target.parentElement.style.backgroundColor =
+//       event.target.dataset.color;
+//   });
 // }
 
-const attrTitle = document.createAttribute('title');
-attrTitle.value = 'new title text for attr';
-unique.setAttributeNode(attrTitle);
+for (const btn of btns) {
+  btn.addEventListener(
+    'click',
+    ({
+      target: {
+        parentElement,
+        dataset: { color },
+      },
+    }) => {
+      parentElement.style.backgroundColor = color;
+    }
+  );
+}
