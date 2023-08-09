8838aa6abfa18aef3de6013aa189f6b238080e55
Olena Zdanovska <freshjs@gmail.com>
2023-08-09 09:44:32 +0300

    Add dispatchEvent

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -2,5 +2,26 @@
 
 const unique = document.getElementById('unique');
 
-unique.onclick = function(){console.log(55)}
-unique.onclick = function(){console.log('qwe')}
+const log44 = () => {
+  console.log(44);
+  unique.removeEventListener('click', log44);
+};
+
+unique.addEventListener('click', log44);
+unique.addEventListener(
+  'click',
+  () => {
+    console.log('qwewqe');
+  },
+  { once: true }
+);
+unique.dispatchEvent(new MouseEvent('click'));
+
+const ownEvent = new Event('myEventType');
+unique.addEventListener('myEventType', () => {
+  console.log('myEvent');
+});
+
+if (Math.random() > 0.5) {
+  unique.dispatchEvent(ownEvent);
+}
