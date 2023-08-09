9d70504442de38e60de43f87794d044cabf345e9
Olena Zdanovska <freshjs@gmail.com>
2023-08-09 12:05:07 +0300

    Start hw data-attributes

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,31 +1,16 @@
 'use strict';
 
-const btns = document.querySelectorAll('section>button');
+//const btns = document.getElementsByClassName('btn-container')[0].children;
 
-// for (const btn of btns) {
-//   btn.addEventListener('click', (event) => {
-//     // console.log(event.target.textContent);
-//     // console.log(event.target.innerText);
-//     // console.log(event.target.innerHTML);
-//     // console.dir(event.target.attributes);
-//     // console.log(event.target.dataset.color);
-//     //console.log(event.target.style);
-//     //прописати фоной колір секції
-//     event.target.parentElement.style.backgroundColor =
-//       event.target.dataset.color;
-//   });
-// }
+const btns = document.querySelectorAll('.btn-container>button');
+const [flexParent] = document.getElementsByClassName('flex-parent');
 
 for (const btn of btns) {
-  btn.addEventListener(
-    'click',
-    ({
-      target: {
-        parentElement,
-        dataset: { color },
-      },
-    }) => {
-      parentElement.style.backgroundColor = color;
+  btn.addEventListener('click', (event) => {
+    flexParent.style.flexDirection = event.target.dataset.flexDirection;
+    for (const btn of btns) {
+      btn.style.color = '';
     }
-  );
+    event.target.style.color = 'red';
+  });
 }
