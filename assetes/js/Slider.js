f81e68a8df4765d1047fcc54101646313c36d208
Olena Zdanovska <freshjs@gmail.com>
2023-08-10 09:55:09 +0300

    Refactor imagesDB & add updateSlide

--- assetes/js/Slider.js
+++ assetes/js/Slider.js
@@ -40,10 +40,22 @@ class Slider {
 }
 
 const imagesDB = [
-  'https://upload.wikimedia.org/wikipedia/commons/2/27/Commander_%28Biden_dog%29_in_February_2022.jpg',
-  'https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1588364974.jpg',
-  'https://dogtime.com/wp-content/uploads/sites/12/2023/07/GettyImages-157603003-e1690769397327.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzU9eNjsEXgiMUr1pFn2riqug-fle6gyJmcHtqSVOuGfXj472raQa8wD9H5a58H1ptsHs&usqp=CAU',
+  [
+    'https://upload.wikimedia.org/wikipedia/commons/2/27/Commander_%28Biden_dog%29_in_February_2022.jpg',
+    'dog 1',
+  ],
+  [
+    'https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1588364974.jpg',
+    'dog 2',
+  ],
+  [
+    'https://dogtime.com/wp-content/uploads/sites/12/2023/07/GettyImages-157603003-e1690769397327.jpg',
+    'dog 3',
+  ],
+  [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzU9eNjsEXgiMUr1pFn2riqug-fle6gyJmcHtqSVOuGfXj472raQa8wD9H5a58H1ptsHs&usqp=CAU',
+    'dog 4',
+  ],
 ];
 
 const slider = new Slider(imagesDB);
@@ -53,13 +65,17 @@ const [prevBtn, nextBtn] = document.querySelectorAll(
   '.slider-container>button'
 );
 
-img.src = slider.currentSlide;
+const updateSlide = () => {
+  img.src = slider.currentSlide[0];
+  img.alt = slider.currentSlide[1];
+};
+updateSlide();
 
 const btnHandler =
   (direction = 'next') =>
   () => {
     slider.currentIndex = slider[direction];
-    img.src = slider.currentSlide;
+    updateSlide();
   };
 
 nextBtn.addEventListener('click', btnHandler('next'));
