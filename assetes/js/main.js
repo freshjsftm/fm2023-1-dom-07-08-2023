6542a07804ff0500c727be2bfec633cf95faf419
Olena Zdanovska <freshjs@gmail.com>
2023-08-16 12:51:42 +0300

    Optimize fetch

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,47 +1,13 @@
 'use strict';
 
-// const propmise = fetch('http://127.0.0.1:5500/assetes/js/data.json');
-// console.log(propmise);
-
-fetch('http://127.0.0.1:5500/assetes/js/data.json')
+const url = 'http://127.0.0.1:5500/assetes/js/data.json';
+fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.table(data);  //users
-    const users = data;
-    //works with users
+    for (const user of data) {
+      console.log(user.firstName);
+    }
   })
   .catch((error)=>{
     console.log(error);
   })
-  .finally(()=>{
-    console.log('finally');
-  })
-
-// const promiseResult = propmise.then((response) => {
-//   const promiseResult = response.json();
-//   return promiseResult;
-// });
-// promiseResult.then((users) => {
-//   console.table(users);
-// });
-
-// propmise.then(
-//   (response) => {
-//     console.log('ok');
-//     const promiseResult = response.json();
-//     promiseResult.then(
-//       (users) => {
-//         console.log('ok data');
-//         console.table(users);
-//       },
-//       (error) => {
-//         console.log('not data');
-//         console.log(error);
-//       }
-//     );
-//   },
-//   (error) => {
-//     console.log('not');
-//     console.log(error);
-//   }
-// );
