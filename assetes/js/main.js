b48ba93c60dc331bc65882a10a109ca900ce4bf8
Olena Zdanovska <freshjs@gmail.com>
2023-08-16 12:05:37 +0300

    Add fetch

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,27 +1,47 @@
 'use strict';
 
-const user = {
-  firstName: 'Brad',
-  lastName: 'Pitt',
-  age: 50,
-  isMale: true,
-  pett: undefined,
-  children: ['Anna', undefined, 'Tom'],
-  address: {
-    town: 'NY',
-    street: 12,
-    home: undefined,
-    qqqq:{
-      prop:12
-    }
-  },
-  ukrainen: null,
-  getFullName() {
-    return this.firstName + ' ' + this.lastName;
-  },
-  [Symbol('my symbol')]: 123,
-};
-//serialize
-const userSerialize = JSON.stringify(user);
-const userDeSerialize = JSON.parse(userSerialize);
+// const propmise = fetch('http://127.0.0.1:5500/assetes/js/data.json');
+// console.log(propmise);
 
+fetch('http://127.0.0.1:5500/assetes/js/data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.table(data);  //users
+    const users = data;
+    //works with users
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+  .finally(()=>{
+    console.log('finally');
+  })
+
+// const promiseResult = propmise.then((response) => {
+//   const promiseResult = response.json();
+//   return promiseResult;
+// });
+// promiseResult.then((users) => {
+//   console.table(users);
+// });
+
+// propmise.then(
+//   (response) => {
+//     console.log('ok');
+//     const promiseResult = response.json();
+//     promiseResult.then(
+//       (users) => {
+//         console.log('ok data');
+//         console.table(users);
+//       },
+//       (error) => {
+//         console.log('not data');
+//         console.log(error);
+//       }
+//     );
+//   },
+//   (error) => {
+//     console.log('not');
+//     console.log(error);
+//   }
+// );
