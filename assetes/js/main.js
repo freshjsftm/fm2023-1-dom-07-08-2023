0a8b0d957f4a3fd0663d7eb812612a369115f703
Olena Zdanovska <freshjs@gmail.com>
2023-08-17 10:22:56 +0300

    Example micro & macro

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -1,61 +1,29 @@
 'use strict';
-
-const myPromise1 = new Promise(executor);
-
-function executor(resolve, reject) {
-  Math.random() > 0.5 ? resolve('data123') : reject('not');
-}
-
-myPromise1
-  .then((data) => {
-    console.log(data);
+console.log('start');
+setTimeout(
+  () => { //macro
+  console.log('setTimeout1');
+}, 0);
+setTimeout(
+  () => { //macro
+  console.log('setTimeout2');
+}, 0);
+const promise = new Promise((resolve, reject) => {
+  console.log('promise start');
+  resolve();
+  reject();
+  console.log('promise end');
+});
+promise
+  .then(
+    () => { //micro
+    console.log('then resolve');
   })
-  .catch((error) => {
-    console.log(error);
-  });
-
-console.log(myPromise1);
-
-// setTimeout(callback, ms)
-
-// timeout(ms).then(callback)
-
-function timeout(ms, msg = 'hi') {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(msg);
-    }, ms);
-  });
-}
-
-timeout(1000, 'qwe')
-  .then((data) => {
-    console.log(data);
+  .catch(() => {
+    console.log('catch reject');
   })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log(3);
+  .finally(
+    () => { //micro
+    console.log('finally');
   });
-
-function loadImage(path) {
-  const img = new Image();
-  img.src = path;
-  return new Promise((resolve, reject) => {
-    img.addEventListener('load', () => {
-      resolve(img);
-    });
-    img.addEventListener('error', () => {
-      reject(new Error('path invalid'));
-    });
-  });
-}
-
-loadImage('https://www.filmibeat.com/img/popcorn/profile_photos/tom-cruise-20190610151455-4602.jpg')
-  .then((elem) => {
-    document.body.append(elem);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+console.log('end');
